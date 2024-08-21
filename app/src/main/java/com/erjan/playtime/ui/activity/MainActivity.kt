53c9381879c78f7f481f607f744056a2348c7987
Эржан Дюшебаев <erjan.d@breez.pro>
2024-08-22 02:07:51 +0600

    minor changes

--- app/src/main/java/com/erjan/playtime/ui/activity/MainActivity.kt
+++ app/src/main/java/com/erjan/playtime/ui/activity/MainActivity.kt
@@ -2,13 +2,12 @@ package com.erjan.playtime.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.erjan.playtime.R
 import com.erjan.playtime.local.PreferencesHelper
 import com.erjan.playtime.time.PlaytimeListOrchestrator
 
-class MainActivity : AppCompatActivity(R.layout.activity_main), LifecycleOwner {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
